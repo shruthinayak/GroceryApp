7a7581975dafae78f5a2f1d5bc983fc9fab6e81a
rn.shruthi@gmail.com <Radhakrishna4>
2015-07-11 16:36:37 -0500

    Retrofit Settings updated

--- app/src/main/java/com/grocery/app/tabswipe/communicate/RetrofitSettings.java
+++ app/src/main/java/com/grocery/app/tabswipe/communicate/RetrofitSettings.java
@@ -3,7 +3,12 @@ package com.grocery.app.tabswipe.communicate;
 
 import android.app.Application;
 
+import com.grocery.app.tabswipe.communicate.services.JsonService;
+import com.grocery.app.tabswipe.communicate.services.SplitwiseTestService;
+import com.grocery.app.tabswipe.utilities.Constants;
+
 import retrofit.RestAdapter;
+
 
 
 /**
@@ -11,18 +16,33 @@ import retrofit.RestAdapter;
  */
 
 public class RetrofitSettings extends Application {
-    private static final String JSON_SERVER = "http://192.168.2.15:9000/";
+
+    private SplitwiseTestService splitwiseTestService;
     private JsonService jsonService;
 
-    public RetrofitSettings() {
+    public RetrofitSettings(int key) {
+        RestAdapter restAdapter;
         // This uses 10.0.2.2 which is how the emulator can address the host operating system.
-        RestAdapter restAdapter = new RestAdapter.Builder().setLogLevel(RestAdapter.LogLevel.FULL).
-                setEndpoint(JSON_SERVER).build();
-        jsonService = restAdapter.create(JsonService.class);
+        switch(key){
+            case Constants.KEY_JSON_SERVER_ID:
+                 restAdapter = new RestAdapter.Builder().setLogLevel(RestAdapter.LogLevel.FULL).
+                        setEndpoint(Constants.URL_JSON_SERVER).build();
+                jsonService = restAdapter.create(JsonService.class);
+                break;
+            case Constants.KEY_SPLITWISE_TEST_ID:
+                 restAdapter = new RestAdapter.Builder().setLogLevel(RestAdapter.LogLevel.FULL).
+                        setEndpoint(Constants.URL_SPLITWISE_TEST).build();
+                splitwiseTestService = restAdapter.create(SplitwiseTestService.class);
+                break;
+        }
+
     }
 
     public JsonService getJsonService() {
         return jsonService;
+    }
+    public SplitwiseTestService getSplitwiseTestServiceService() {
+        return splitwiseTestService;
     }
 }
 
