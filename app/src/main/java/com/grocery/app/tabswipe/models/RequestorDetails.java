3e2058dc1bf53ca315e02299b9cd5c573be3e256
rn.shruthi@gmail.com <Radhakrishna4>
2015-07-13 09:57:40 -0500

    Enhance the data models to match the JSON fields

--- app/src/main/java/com/grocery/app/tabswipe/models/RequestorDetails.java
+++ app/src/main/java/com/grocery/app/tabswipe/models/RequestorDetails.java
@@ -6,20 +6,22 @@ import java.util.List;
  * Created by SG0222540 on 7/10/2015.
  */
 public class RequestorDetails {
-    public String getItemId() {
-        return itemId;
+    public String getItm_id() {
+        return itm_id;
     }
 
-    public void setItemId(String itemId) {
-        this.itemId = itemId;
+    public void setItm_id(String itm_id) {
+        this.itm_id = itm_id;
     }
 
-    String itemId;
+    String itm_id;
+    String itm_name;
     String totalQty;
     List<Requestor> details;
 
-    public RequestorDetails(String itemId, String totalQty, List<Requestor> details) {
-        this.itemId = itemId;
+    public RequestorDetails(String itemName, String itemId, String totalQty, List<Requestor> details) {
+        this.itm_id = itemId;
+        this.itm_name = itemName;
         this.totalQty = totalQty;
         this.details = details;
     }
@@ -29,4 +31,12 @@ public class RequestorDetails {
     return details;
     }
 
+    public boolean areAllItemsLocked(){
+        for(Requestor r: details){
+            if(!r.isLock()){
+                return false;
+            }
+        }
+        return true;
+    }
 }
